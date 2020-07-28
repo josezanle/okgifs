e5ce15d03f3265ea4e75ee30a61957aa89c6dae9
josezanle <63666176+josezanle@users.noreply.github.com>
2020-07-27 21:24:38 -0300

    realizacion de proyecto con React

---
+++ precache-manifest.de48869755471184ae93f8fdcc50391e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cafa94e3755a58739c94bbf1486c7fe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5b762e6edf54579a5f3",
+    "url": "/static/css/main.619fb600.chunk.css"
+  },
+  {
+    "revision": "5ab1a84e0cea5859c638",
+    "url": "/static/js/2.2bd774b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2bd774b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b762e6edf54579a5f3",
+    "url": "/static/js/main.a59228a8.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
